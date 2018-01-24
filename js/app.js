d4be44ad57b8cbd8c62b09d67fa6949bd837e95f
Hovhannes Galstyan <galstyan.hovhannes@gmail.com>
2018-01-23 17:51:45 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,3 @@
-// <div class="project 11 grid__col--6">
-//   <h3>React Gallery</h3>
-//   <img class="img--frame" src="img/projects/project-11.png " alt="React Gallery image"/>
-//   <p>React is a JavaScript library for building user interfaces. It's fast, easy and manageable.
-//      For first time it's a pain in some lower body part. In this project I'm using Flickrs API and displaying photo via search or by pushing the buttons.</p>
-//   <h4>Skills</h4>
-//   <ul class="grid__col--12 ">
-//     <li class="skills-html ">HTML</li>
-//     <li class="skills-css ">CSS</li>
-//     <li class="skills-js ">JS</li>
-//     <li class="skills-react ">React</li>
-//   </ul>
-//   <div class="project-nav">
-//     <a href="">
-//       <div class="nav__link">
-//         <img src="img/github.svg" class="nav__link--item" alt="giyhub logo"/>
-//         <p class="nav__link--item">REPO</p>
-//       </div>
-//     </a>
-//     <a href="">
-//       <div class="nav__link">
-//         <img src="img/play.svg" class="nav__link--item" alt="play logo"/>
-//         <p class="nav__link--item">VIEW</p>
-//       </div>
-//     </a>
-//   </div>
-// </div>
-
 const projects = document.querySelector("#projects");
 const skills = document.querySelector(".skills");
 const hamburger = document.querySelector(".hamburger");
@@ -40,7 +12,7 @@ const data = {
   projects: [
     {
       name: "Responsive Layout",
-      src: "img/projects/project-2.png",
+      src: "img/projects/project-2.PNG",
       description: "There are a lot of different devices, with different display sizes. That's why responsive design is important part of a modern web development. In two words responsive website is a website, wich is good looking on every device. I hope like this one.",
       skills: ["HTML", "CSS"],
       repo: "https://github.com/hgalstyan/responsive_layout",
@@ -48,7 +20,7 @@ const data = {
     },
     {
       name: "Online Registration",
-      src: "img/projects/project-3.png",
+      src: "img/projects/project-3.PNG",
       description: "When you are subscribing or registering, you need to fill a form with your info. Well, here it is.",
       skills: ["HTML", "CSS"],
       repo: "https://github.com/hgalstyan/online_registration_v3",
@@ -56,7 +28,7 @@ const data = {
     },
     {
       name: "Photo Gallery",
-      src: "img/projects/project-4.png",
+      src: "img/projects/project-4.PNG",
       description: "There are a lot of great frameworks. I'm using one of them here to display photos.",
       skills: ["HTML", "CSS","JS","JQ"],
       repo: "https://github.com/hgalstyan/photo_gallery_v4",
@@ -64,7 +36,7 @@ const data = {
     },
     {
       name: "Style Guide",
-      src: "img/projects/project-5.png",
+      src: "img/projects/project-5.PNG",
       description: "If you search the internet, you'll find a ton of different web-styles. With guide of course.",
       skills: ["HTML", "CSS","SCSS"],
       repo: "https://github.com/hgalstyan/style_guide_v5",
@@ -72,7 +44,7 @@ const data = {
     },
     {
       name: "Video Player",
-      src: "img/projects/project-6.png",
+      src: "img/projects/project-6.PNG",
       description: "Also great framework but this time it's a video player. Who doesn't want a good looking video player with closed captions. Just in case it's also interactive",
       skills: ["HTML", "CSS","JS","JQ"],
       repo: "https://github.com/hgalstyan/video_player_v6",
@@ -80,7 +52,7 @@ const data = {
     },
     {
       name: "Game Show App",
-      src: "img/projects/project-7.png",
+      src: "img/projects/project-7.PNG",
       description: "Let's play some game. You should guess the word, by clicking on the letters, placed on display. You can miss only five times. Good luck.",
       skills: ["HTML", "CSS","JS","JQ"],
       repo: "https://github.com/hgalstyan/game_show_app_v7",
@@ -88,7 +60,7 @@ const data = {
     },
     {
       name: "SVG Animations",
-      src: "img/projects/project-8.png",
+      src: "img/projects/project-8.PNG",
       description: "Is's a webpage about corgi dogs. All SVG are animated here. Just try. The goal of this project was SVG creation and animation.",
       skills: ["HTML", "CSS"],
       repo: "https://github.com/hgalstyan/svg_animations_v8",
@@ -96,7 +68,7 @@ const data = {
     },
     {
       name: "Web App Dashboard",
-      src: "img/projects/project-9.png",
+      src: "img/projects/project-9.PNG",
       description: "With chart.js, SASS, and JS, I created this dashboard. It's also have local memory. So changes in settings will be saved.",
       skills: ["HTML", "CSS","SCSS","JS","JQ"],
       repo: "https://github.com/hgalstyan/web-app-dashboard",
@@ -104,7 +76,7 @@ const data = {
     },
     {
       name: "Public API",
-      src: "img/projects/project-10.png",
+      src: "img/projects/project-10.PNG",
       description: "API(Application Programming Interface) is a tool which web developers are using to communicate with the servers. This project is an example of that. It's getting random 12 people from another server and displaying them.",
       skills: ["HTML", "CSS", "JS", "JQ"],
       repo: "https://github.com/hgalstyan/public-API-2",
@@ -112,7 +84,7 @@ const data = {
     },
     {
       name: "Flicr Gallery",
-      src: "img/projects/project-11.png",
+      src: "img/projects/project-11.PNG",
       description: "React is a JavaScript library for building user interfaces. It's fast, easy and manageable. For first time it's a pain in some lower body part. In this project I'm using Flickrs API and displaying photo via search or by pushing the buttons.",
       skills: ["HTML", "CSS","JS","React"],
       repo: "https://github.com/hgalstyan/React-gallery",
